394ab043e6646a0bc7b26960d9a4269f10fdb49b
nazgull08 <nazgull08@protonmail.com>
2024-06-03 09:21:50 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    // Создаем конфигурацию клиента с альтернативным сервером
     let config = ClientConfig {
         network: ton_client::net::NetworkConfig {
             server_address: Some("https://venom-testnet.evercloud.dev/d9636056bed3470aa1f4cf343451c9d3/graphql".to_string()),
@@ -16,10 +15,8 @@ async fn main() {
     };
     let context = Arc::new(ClientContext::new(config).unwrap());
 
-    // Адрес контракта, который нужно проверить
-    let address = "UQC38O5AB3-eLN8EfwCZ53by3oychSb49ycWv9J_tiP-owpZ";
+    let address = "qwe";
 
-    // Параметры запроса для проверки баланса
     let query = ParamsOfQueryCollection {
         collection: "accounts".to_string(),
         filter: Some(json!({"id": { "eq": address }})),
@@ -28,7 +25,6 @@ async fn main() {
         limit: None,
     };
 
-    // Выполняем запрос
     match ton_client::net::query_collection(context.clone(), query).await {
         Ok(result) => {
             if let Some(account) = result.result.first() {
